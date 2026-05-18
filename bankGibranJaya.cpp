6c38948e6df3d6566633e4a4bb169ee6a8f8f460
faisal01suryaa <faisal01suryaa@gmail.com>
2026-05-18 13:11:25 +0700

    menambahkan kelas rekeningpremium yang mewarisi dari kelas rekeningbank dengan potongadmin

--- bankGibranJaya.cpp
+++ bankGibranJaya.cpp
@@ -35,3 +35,43 @@ public:
         cout << "Biaya admin Rp 15.000 dipotong" << endl;
     }
 };
+
+class RekeningPremium : public RekeningBank {
+public:
+    RekeningPremium(double s) : RekeningBank(s) {}
+
+    void potongAdmin() {
+        if (saldo > 10000000) {
+            cout << "Nasabah bebas biaya admin" << endl;
+        } else {
+            saldo -= 50000;
+            cout << "Biaya admin Rp 50.000 dipotong" << endl;
+        }
+    }
+};
+
+int main() {
+    RekeningBank* rekening;
+
+    RekeningSyariah syariah(5000000);
+    RekeningKonvensional konvensional(5000000);
+    RekeningPremium premium(8000000);
+
+    rekening = &syariah;
+    rekening->potongAdmin();
+    rekening->tampilSaldo();
+
+    cout << endl;
+
+    rekening = &konvensional;
+    rekening->potongAdmin();
+    rekening->tampilSaldo();
+
+    cout << endl;
+
+    rekening = &premium;
+    rekening->potongAdmin();
+    rekening->tampilSaldo();
+
+    return 0;
+}
